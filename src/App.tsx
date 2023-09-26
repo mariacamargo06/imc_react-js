e049322ac83393236faccdce0ed7ddba9005df63
mariacamargo06 <135022264+mariacamargo06@users.noreply.github.com>
2023-09-26 08:59:30 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,83 @@
-import styles from './App.module.css'
-import poweredImage from './assets/powered.png'
-
-
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+import {levels, calculateImc,Level} from './helpers/imc';
+import { GridItem } from './components/GridItem'
+import seta from './assets/leftarrow.png'
 
 const App = () => {
-  return (
+  
+  const [weight, setWeight] = useState(0);
+  const [heigth, setHeight] = useState(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
 
+  const handleCalc = () => {
+    if (heigth && weight) {
+      setToShow(calculateImc(heigth,weight));
+      
+    }
+    else {
+      alert("Informe a Altura e o Peso")
+    }
+  }
+
+const handleBack = () =>{
+  setToShow(null);
+  setHeight(0);
+  setWeight(0);
+
+}
+
+  return(
     <div className={styles.main}>
-      <header>
-        <div className={styles.headerContainer}>
-          <img src={poweredImage} alt="" width={150} />
-        </div>
-      </header>
+        <header>
+            <div className={styles.headerContainer}>
+              <img src={poweredImage} alt="" width={150} />
+            </div>
+        </header>
+        <div className={styles.container}>
+            <div className={styles.leftSide}>
+              <h1>Calculo do IMC</h1>
+              <p>O IMC (Índice de Massa Corporal) é reconhecido pela OMS (Organização Mundial da Saúde) como um padrão internacional que avalia se as pessoas, entre 20 e 59 anos, estão com peso ideal ou em excesso, em relação a sua altura. </p>
+              <input 
+                type="number"
+                placeholder="Informe a altura. Ex. 1.5 (em metro)"
+                value={heigth> 0 ? heigth:''}
+                onChange={t => setHeight(parseFloat(t.target.value) )}
+              />
 
-      <div className={styles.container}>
-        
-        <div className={styles.leftSide}>
-          <h1>Calculo de IMC</h1>
-          <p>O Índice de Massa Corporal (IMC) é reconhecido pela Organização Mundial da Saúde (OMS) como um padrão internacional que avalia se as pessoas, entre 20 e 59 anos, estão com peso ideal ou em excesso, em relação a sua altura. No entanto, vale lembrar que o resultado não indica como está o estado nutricional.</p>
+              <input 
+                type="number"
+                placeholder="Informe o peso. Ex. 60.5 (em kilo)"
+                value={weight > 0 ? weight:''}
+                onChange={t => setWeight(parseFloat(t.target.value) )}
+              />
 
-          <input type="number"
-            placeholder="Informe sua altura.Ex.1.50"
-          />
+              <button onClick={handleCalc}>Calcular</button>
 
-          <input type="number"
-            placeholder="Informe seu Peso.Ex.50.3"
-
-          />
-          <button>calcular</button>
-
-
-        </div>
-        
-        <div className={styles.rightSide}>
-          ...
-        </div>
-
-      </div>
-
+            </div>
+            
+            <div className={styles.rightSide}>
+              {!toShow &&
+              <div className={styles.grid}>
+                 {levels.map( (item) => (
+                  <GridItem item={item}/>
+                 ) )}
+              </div>
+}
+{
+  toShow &&
+  <div className={styles.rightbig}>
+  <div className={styles.rightArrow} onClick={handleBack}>
+    <img src={seta} width={25}/>
+  </div>
+  <GridItem item={toShow}/>
+</div>
+}
+    </div>
+    </div>
     </div>
   )
-  
 }
+
 export default App;
--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -1,7 +1,7 @@
 export type Level = {
     title: string;
     color: string;
-    icon: 'down' | 'up';
+    icon: string;
     imc: number[];
     yourImc?: number;
 }
@@ -10,11 +10,11 @@ export const levels: Level[] =  [
   {title: 'Magreza',   color:'#96A3AB', icon:'down', imc:[0, 18.5]},
   {title: 'Normal',    color:'#0EAD69', icon:'up',   imc:[18.6, 24.9]},
   {title: 'Sobrepeso', color:'#E2B039', icon:'down', imc:[25, 30]},
-  {title: 'Obesidade ', color:'#C3423F', icon:'down', imc:[30.1, 99]}
+  {title: 'Obesidade', color:'#C3423F', icon:'down', imc:[30.1, 99]}
 ];
 
-export const calculateImc = (altura:number, peso:number) => {
-    const imc = peso / (altura * altura);
+export const calculateImc = (height:number, weight:number) => {
+    const imc = weight / (height * height);
 
     for ( let i in levels) {
       if ( imc >= levels[i].imc[0] && imc <= levels[i].imc[1]){
@@ -24,3 +24,4 @@ export const calculateImc = (altura:number, peso:number) => {
     }
     return null;  
 }
+
